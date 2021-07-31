a596a23a599cbe9dc822e79369ab4992c4bf65dd
Catherine De Mesa <59064455+xCoderSai@users.noreply.github.com>
2021-07-30 22:22:56 -0500

    Create sortedsquares.js

---
+++ sortedsquares.js
@@ -0,0 +1,15 @@
+function sortedSquaredArray(array) {
+	const sortedSquares = new Array(array.length).fill(0);
+	
+	for(let idx=0; idx < array.length; idx++) {
+		const value = array[idx];
+		sortedSquares[idx] = value * value;
+	}
+	sortedSquares.sort((a, b) => a - b);
+	return sortedSquares;
+	
+	
+}
+
+// Do not edit the line below.
+exports.sortedSquaredArray = sortedSquaredArray;
