6c7b1cc9875be19d87880dadca346f18ca8ea63e
Catherine De Mesa <59064455+xCoderSai@users.noreply.github.com>
2021-07-30 22:23:52 -0500

    EASY

--- sortedsquares.js
+++ sortedsquares.js
@@ -1,3 +1,9 @@
+//Write a function that takes in a non-empty array of integers that are sorted
+//in ascending order and returns a new array of the same length with the squares
+//of the original integers also sorted in ascending order.
+
+
+
 function sortedSquaredArray(array) {
 	const sortedSquares = new Array(array.length).fill(0);
 	
